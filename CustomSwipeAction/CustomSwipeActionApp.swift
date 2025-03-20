976623832236cc1a9be2d0e1f2e484c5cf872889
katafuchix <katafuchix@gmail.com>
2025-03-20 11:37:37 +0900

    Initial Commit

---
+++ CustomSwipeAction/CustomSwipeActionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomSwipeActionApp.swift
+//  CustomSwipeAction
+//
+//  Created by cano on 2025/03/20.
+//
+
+import SwiftUI
+
+@main
+struct CustomSwipeActionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
